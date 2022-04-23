833a957ef6ce503ca9297c0764a5fc055ad76df8
karan <karantnikita@gmail.com>
2022-04-23 10:14:14 +0200

    23.04.2022 made it look good

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import fileImport
 import regressionCalculaton
 import reportCreator
 import strategyStatistics
+from binanceApiTradeInfo.apiInitialization import clientInit, getDateOfTradesToReceive
 from deltaRangeFinder.decTreeRangesCombine import weightSearch, weightLinesDepthSearch, \
     featuresListFinder, featuresFinalReport, featuresCombineFinal
 from deltaRangeFinder.deltaFindML import decorator, decisionTree
@@ -10,41 +11,57 @@ from deltaRangeFinder.deltaFindML import decorator, decisionTree
 @decorator
 def main():
     repFile = 'data/dr13k.txt'
-    df = fileImport.csvImport(repFile)[0]
-    colNames = fileImport.csvImport(repFile)[1]
-    ratioData = strategyStatistics.getRatio(df, colNames)
-    stratData = strategyStatistics.getStatForStrat(df, colNames)
-    regrDict = regressionCalculaton.regressionValues(stratData, colNames)[0]
-    listOfReqVal = regressionCalculaton.regressionValues(stratData, colNames)[1]
-    stratRatioDict = strategyStatistics.strategyGetRatio(stratData)
+    df = fileImport.csvImport(repFile)[0]  # our dataset from imported report
+    colNames = fileImport.csvImport(repFile)[1]  # column name from imported file
+    ratioData = strategyStatistics.getRatio(df, colNames)  # finds basic info as PnL or number of + and - trades
+    stratData = strategyStatistics.getStatForStrat(df, colNames)  # all trades for every strategy separated
+    regrDict = regressionCalculaton.regressionValues(stratData, colNames)[0]  # regressions for every parame in strat
+    listOfReqVal = regressionCalculaton.regressionValues(stratData, colNames)[1]  # list of values used in program
+    stratRatioDict = strategyStatistics.strategyGetRatio(stratData)  # basic info but for every strategy by themselves
 
     # reportCreator.reportCreation(repFile, stratData, regrDict, ratioData, colNames, df, listOfReqVal,
     # stratRatioDict)
+    # creates a txt report for all file and every strategy(correlation, basic stats, time in trade and so on)
 
     # decisionTree(stratData, colNames)
+    # generates trees, also can turn on feature of creating Jpg or TXT reports
+
     # mode = 0
     mode = 1
+
     weightDict = weightSearch(stratData, listOfReqVal, mode)[0]
-    # print(weightDict)
+    # list of weights to find location and depth
     weightLine = weightSearch(stratData, listOfReqVal, mode)[1]
-    # print(weightLine)
+    # in which line of report there is searched weight
     weightDepthIndex = weightLinesDepthSearch(weightDict, weightLine, mode)
-    # print(weightDepthIndex)
+    # depth of lines with weights
     featureListDict = featuresListFinder(weightDepthIndex, weightLine, weightDict, mode)
-    # print(featureListDict)
+    # returns dictionary with all features on the way to weights in given line (weightLine)
+    # and depth (weightDepthIndex)
 
-    rangesDictFinal = featuresCombineFinal(featuresFinalReport(featureListDict, listOfReqVal), listOfReqVal)
+    # TODO SERVICE FUNCTION FOR ALL RAW FEATURES FOR SELECTED WEIGHTS
     # reportCreator.featureReportCreation(repFile, weightDict, featureListDict)
+    # creates txt report with features in raw format for every of 3 biggest weights in every strategy
+
+    # TODO REMAKE OF DATA ABOUT RANGES AND TXT REPORT ABOUT IT
+    # rangesDictFinal = featuresCombineFinal(featuresFinalReport(featureListDict, listOfReqVal), listOfReqVal)
+    # returns ranges in [$;$] format for all strategies
     # reportCreator.rangesDictFinalReportCreation(repFile, rangesDictFinal)
-    strategyStatistics.bestStrategies(stratRatioDict, regrDict, rangesDictFinal, stratData, listOfReqVal)
+    # creates txt report with all params ranges (not sorted)
 
-    biggestRRList = strategyStatistics.bestStrategies(stratRatioDict, regrDict,
-                                                      rangesDictFinal, stratData, listOfReqVal)[0]
+    # TODO FINDS BEST STRATEGIES
+    # biggestRRList = strategyStatistics.bestStrategies(stratRatioDict, regrDict,
+    #  rangesDictFinal, stratData, listOfReqVal)[0]
+    # biggestRegrDictStrat = strategyStatistics.bestStrategies(stratRatioDict, regrDict,
+    # rangesDictFinal, stratData, listOfReqVal)[1]
+    # finds five strategies with more than 400 trades and highest profit/loss ratio
+    # reportCreator.rangesReportFiveBestCreation(repFile, rangesDictFinal, biggestRRList,
+    # biggestRegrDictStrat, stratRatioDict)
+    # creates txt report with ranges and correlations for five strategies with highest ratio
 
-    biggestRegrDictStrat = strategyStatistics.bestStrategies(stratRatioDict, regrDict,
-                                                             rangesDictFinal, stratData, listOfReqVal)[1]
+    clientInit()  # init of work with binance apI
+    getDateOfTradesToReceive(stratData)
 
-    reportCreator.rangesReportFiveBestCreation(repFile, rangesDictFinal, biggestRRList, biggestRegrDictStrat)
 
 if __name__ == '__main__':
     main()
